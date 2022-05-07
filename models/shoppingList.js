fbcbc037bce6659fda6715c91d070bcfbcab1d97
Jason Vance <jbvance@gmail.com>
2022-05-07 15:10:02 -0500

    refactor add to shopping list

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -7,6 +7,32 @@ const shoppingListSchema = new Schema({
     {
       id: { type: String, required: true },
       label: { type: String, required: true },
+      calories: { type: Number },
+      cautions: [{ type: String }],
+      cuisineType: [{ type: String }],
+      dietLabels: [{ type: String }],
+      dishType: [{ type: String }],
+      healthLabels: [{ type: String }],
+      image: { type: String },
+      images: {
+        REGULAR: {
+          height: { type: Number },
+          width: { type: Number },
+          url: { type: String },
+        },
+        SMALL: {
+          height: { type: Number },
+          width: { type: Number },
+          url: { type: String },
+        },
+        THUMBNAIL: {
+          height: { type: Number },
+          width: { type: Number },
+          url: { type: String },
+        },
+      },
+      ingredientLines: [{ type: String }],
+      digest: [{ type: Object }],
       ingredients: [
         {
           food: { type: String },
@@ -19,6 +45,15 @@ const shoppingListSchema = new Schema({
           weight: { type: Number },
         },
       ],
+      mealType: [{ type: String }],
+      shareAs: { type: String },
+      source: { type: String },
+      totalDaily: { type: Object },
+      totalNutrients: { type: Object },
+      totalTime: { type: Number },
+      totalWeight: { type: Number },
+      uri: { type: String },
+      url: { type: String },
       creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
     },
   ],
--- models/user.js
+++ models/user.js
@@ -12,6 +12,32 @@ const userSchema = new Schema({
       {
         id: { type: String, required: true },
         label: { type: String, required: true },
+        calories: { type: Number },
+        cautions: [{ type: String }],
+        cuisineType: [{ type: String }],
+        dietLabels: [{ type: String }],
+        dishType: [{ type: String }],
+        healthLabels: [{ type: String }],
+        image: { type: String },
+        images: {
+          REGULAR: {
+            height: { type: Number },
+            width: { type: Number },
+            url: { type: String },
+          },
+          SMALL: {
+            height: { type: Number },
+            width: { type: Number },
+            url: { type: String },
+          },
+          THUMBNAIL: {
+            height: { type: Number },
+            width: { type: Number },
+            url: { type: String },
+          },
+        },
+        ingredientLines: [{ type: String }],
+        digest: [{ type: Object }],
         ingredients: [
           {
             food: { type: String },
@@ -21,15 +47,24 @@ const userSchema = new Schema({
             measure: { type: String },
             quantity: { type: Number },
             text: { type: String },
-            weight: { type: Number }
-          }
-        ]
-      }
-    ]
+            weight: { type: Number },
+          },
+        ],
+        mealType: [{ type: String }],
+        shareAs: { type: String },
+        source: { type: String },
+        totalDaily: { type: Object },
+        totalNutrients: { type: Object },
+        totalTime: { type: Number },
+        totalWeight: { type: Number },
+        uri: { type: String },
+        url: { type: String },
+      },
+    ],
   },
   favorites: [
-    { type: mongoose.Types.ObjectId, required: true, ref: 'Favorite' }
-  ]
+    { type: mongoose.Types.ObjectId, required: true, ref: 'Favorite' },
+  ],
 });
 
 userSchema.plugin(uniqueValidator);
